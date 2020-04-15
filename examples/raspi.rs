9eec8739514f3f9b4f1d3d1a74cf1e6b95c2d487
Mikko Viitamäki <mikko.viitamaki@gmail.com>
2020-04-15 14:46:50 +0300

    sleeep a sec

--- examples/raspi.rs
+++ examples/raspi.rs
@@ -1,11 +1,11 @@
 extern crate linux_embedded_hal as hal;
 extern crate isl229125;
-
+use std::{thread, time};
 
 fn main(){
     let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
     let mut led_counter = isl229125::Isl29125::new(dev);
-    led_counter.set_operating_mode(isl229125::OperationModes::Green_Red_Blue).unwrap();
+    led_counter.set_operating_mode(isl229125::OperationModes::GreenOnly).unwrap();
     let id = led_counter.verify_device_id();
     match id{
         Ok(id) =>{ 
@@ -20,6 +20,7 @@ fn main(){
                     }
                     Err(_) => println!("Cant read counters"),
                 }
+                thread::sleep(time::Duration::from_secs(1));
             }
 
 
